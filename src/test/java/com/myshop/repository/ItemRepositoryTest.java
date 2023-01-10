5cfbd244390e1b87ba520ab6bb6d7e8747bebf5a
klina8575 <klina8575@gmail.com>
2023-01-10 16:56:03 +0900

    up

---
+++ src/test/java/com/myshop/repository/ItemRepositoryTest.java
@@ -0,0 +1,93 @@
+package com.myshop.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import com.myshop.constant.ItemSellStatus;
+import com.myshop.entity.Item;
+
+@SpringBootTest
+@TestPropertySource(locations="classpath:application-test.properties")
+class ItemRepositoryTest {
+	
+	@Autowired
+	ItemRepository itemRepository;
+	
+	/*
+	public void createItemTest() {
+		Item item = new Item();
+		item.setItemNm("테스트 상품");
+		item.setPrice(10000);
+		item.setItemDetail("테스트 상품 상세 설명");
+		item.setItemSellStatus(ItemSellStatus.SELL);
+		item.setStockNumber(100);
+		item.setRegTime(LocalDateTime.now());
+		item.setUpdateTime(LocalDateTime.now());
+		
+		Item savedItem = itemRepository.save(item); //데이터 insert
+		
+		System.out.println(savedItem.toString());
+	}*/
+	
+	public void createItemTest() {
+		for (int i=1; i<=10; i++) {			
+			Item item = new Item();
+			item.setItemNm("테스트 상품" + i);
+			item.setPrice(10000 + i);
+			item.setItemDetail("테스트 상품 상세 설명" + i);
+			item.setItemSellStatus(ItemSellStatus.SELL);
+			item.setStockNumber(100);
+			item.setRegTime(LocalDateTime.now());
+			item.setUpdateTime(LocalDateTime.now());	
+			Item savedItem = itemRepository.save(item); //데이터 insert
+		}
+	}
+	
+	@Test
+	@DisplayName("상품명 조회 테스트")
+	public void findByItemNmTest() {
+		this.createItemTest(); //item 테이블에 insert
+		List<Item> itemList = itemRepository.findByItemNm("테스트 상품2");
+		for (Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+	
+	@Test
+	@DisplayName("상품명, 상품상세설명 or 테스트")
+	public void findByItemNmOrItemDetailTest() {
+		this.createItemTest();
+		List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+		for (Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+	
+	@Test
+	@DisplayName("가격 LessThan 테스트")
+	public void findByPriceLessThanTest() {
+		this.createItemTest();
+		List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+		for (Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+	
+	@Test
+	@DisplayName("가격 내림차순 조회 테스트")
+	public void findByPriceLessThanOrderByPriceDescTest() {
+		this.createItemTest();
+		List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+		for (Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+}
