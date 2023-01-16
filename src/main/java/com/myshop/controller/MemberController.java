016f9c5e999d881a7f47490e8d1e8e1e7581738b
klina8575 <klina8575@gmail.com>
2023-01-16 15:11:49 +0900

    up

--- src/main/java/com/myshop/controller/MemberController.java
+++ src/main/java/com/myshop/controller/MemberController.java
@@ -1,5 +1,39 @@
 package com.myshop.controller;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.myshop.dto.MemberFormDto;
+import com.myshop.entity.Member;
+import com.myshop.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/members")
+@Controller
+@RequiredArgsConstructor
 public class MemberController {
+	private final MemberService memberService;
+	private final PasswordEncoder passwordEncoder;
+	
+	//회원가입 화면 
+	@GetMapping(value = "/new")
+	public String memberForm(Model model) {
+		model.addAttribute("memberFormDto", new MemberFormDto());
+		return "member/memberForm";
+	}
+	
+	//회원가입 버튼을 눌렀을때 실행되는 메소드
+	@PostMapping(value = "/new")
+	public String memberForm(MemberFormDto memberFormDto) {
+		Member member = Member.createMember(memberFormDto, passwordEncoder);
+		memberService.saveMember(member);
+		
+		return "redirect:/";
+	}
 
 }
