8a2857cb72d6c8ecb6c7f195994114e648e347fa
klina8575 <klina8575@gmail.com>
2023-01-19 12:25:01 +0900

    up

--- src/test/java/com/myshop/repository/ItemRepositoryTest.java
+++ src/test/java/com/myshop/repository/ItemRepositoryTest.java
@@ -61,7 +61,7 @@ class ItemRepositoryTest {
 			item.setItemSellStatus(ItemSellStatus.SELL);
 			item.setStockNumber(100);
 			item.setRegTime(LocalDateTime.now());
-			item.setUpdateTime(LocalDateTime.now());	
+//			item.setUpdateTime(LocalDateTime.now());	
 			Item savedItem = itemRepository.save(item); //데이터 insert
 		}
 	}
@@ -75,7 +75,7 @@ class ItemRepositoryTest {
 			item.setItemSellStatus(ItemSellStatus.SELL);
 			item.setStockNumber(100);
 			item.setRegTime(LocalDateTime.now());
-			item.setUpdateTime(LocalDateTime.now());	
+//			item.setUpdateTime(LocalDateTime.now());	
 			Item savedItem = itemRepository.save(item); //데이터 insert
 		}
 		
@@ -87,7 +87,7 @@ class ItemRepositoryTest {
 			item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
 			item.setStockNumber(0);
 			item.setRegTime(LocalDateTime.now());
-			item.setUpdateTime(LocalDateTime.now());	
+//			item.setUpdateTime(LocalDateTime.now());	
 			Item savedItem = itemRepository.save(item); //데이터 insert
 		}
 	}
