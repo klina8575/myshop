e42e40886bdbbbe0606f1c9ed1be781d50d2107f
klina8575 <klina8575@gmail.com>
2023-01-25 17:09:10 +0900

    up

--- src/main/java/com/myshop/entity/Item.java
+++ src/main/java/com/myshop/entity/Item.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import javax.persistence.*;
 import com.myshop.constant.ItemSellStatus;
 import com.myshop.dto.ItemFormDto;
+import com.myshop.exception.OutOfStockException;
 
 import lombok.*;
 
@@ -43,5 +44,16 @@ public class Item extends BaseEntity {
 		this.stockNumber = itemFormDto.getStockNumber();
 		this.itemDetail = itemFormDto.getItemDetail();
 		this.itemSellStatus = itemFormDto.getItemSellStatus();
+	}
+	
+	//상품의 재고 감소
+	public void removeStock(int stockNumber) {
+		int restStock = this.stockNumber - stockNumber; //주문 후 남은 재고수량
+		
+		if(restStock < 0) {
+			throw new OutOfStockException("상품의 재고가 부족 랍니다. (현재 재고 수량:" + this.stockNumber + ")");
+		}  
+		
+		this.stockNumber = restStock; //주문 후 남은 재고수량을 상품의 현재 재고 값으로 할당
 	}
 }
